0f474c883d81f454d422b6f5756dce30d3485e5a
jangya0820 <43634001+jangya0820@users.noreply.github.com>
2018-11-07 13:11:52 +0900

    Initial commit

---
+++ 로그인lock풀어주는쿼리.sql
@@ -0,0 +1,27 @@
+-- 전체 조회
+SELECT ADMIN_ID
+     , ADMIN_PWD
+     , NAME
+     , FAIL_CNT
+  FROM HOME_ADMIN;
+
+-- admin, test 만 조회
+SELECT ADMIN_ID
+     , ADMIN_PWD
+     , NAME
+     , FAIL_CNT
+  FROM HOME_ADMIN
+ WHERE ADMIN_ID IN ('admin', 'test');
+
+-- id 여러 개 FAIL_CNT를 0으로 update
+UPDATE HOME_ADMIN
+  SET FAIL_CNT = 0
+ WHERE ADMIN_ID IN ('test', 'info');
+
+-- update (최종)
+-- FAIL_CNT는 0으로 초기화
+-- ADMIN_PWD는 1111로 초기화
+UPDATE HOME_ADMIN
+  SET FAIL_CNT = 0						-- 비밀번호 틀린 횟수 0으로 초기화
+    , ADMIN_PWD = 1111					-- password 초기화
+ WHERE ADMIN_ID = :P_FAILCNT_ID;	-- 해당 id 입력
